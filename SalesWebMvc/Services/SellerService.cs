a5b564493089a8a057e3afa7544a64f02f51ef55
Vinicius Silva <vsaguiar@outlook.com>
2023-05-18 22:10:40 -0300

    Serviço de operação relacionado a vendedor - SellerService

---
+++ SalesWebMvc/Services/SellerService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SalesWebMvc.Models;
+
+namespace SalesWebMvc.Services
+{
+    public class SellerService
+    {
+        private readonly SalesWebMvcContext _context;
+
+        // Construtor para que a injeção de dependência possa ocorrer
+        public SellerService(SalesWebMvcContext context)
+        {
+            _context = context;
+        }
+
+        public List<Seller> FindAll()
+        {
+            return _context.Seller.ToList();
+        }
+
+    }
+}
